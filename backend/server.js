d0ff3d4a57917235629a27f1805613eee2a5cc95
Yaman Sangwan <yamansangwan.cse@gmail.com>
2026-06-18 23:54:33 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config()
-
-const app = require("./src/app")
-
-const connectDB = require("./src/database/db")
-connectDB()
-
-app.listen(3000 , () => console.log("backend server is running") )
